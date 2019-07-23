bf98eb9e0e7eab31e3c16876c83f404383e8b37e
Patrick Flynn <sqllensman@outlook.com>
2019-07-23 22:40:56 +1000

    Adding Minium Version as Only support 2005 or higher

--- functions/Get-DbaServerRole.ps1
+++ functions/Get-DbaServerRole.ps1
@@ -60,7 +60,7 @@ function Get-DbaServerRole {
     process {
         foreach ($instance in $SqlInstance) {
             try {
-                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
+                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 9
             } catch {
                 Stop-Function -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
